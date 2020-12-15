82bf1bd82e917aebfaebfb4a22c6dc3075e34ce5
Henning <henning@blueshell.com>
2020-12-15 19:17:37 +0100

    Fix name

--- SnappyWinscard/Winscard.cs
+++ SnappyWinscard/Winscard.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace CardTest
+namespace SnappyWinscard
 {
     public class Winscard
     {
@@ -544,22 +544,22 @@ namespace CardTest
         /// <summary>
         /// There is no active protocol.
         /// </summary>
-        public const Int16 SCARD_PROTOCOL_UNDEFINED = 0x0000;
+        public const short SCARD_PROTOCOL_UNDEFINED = 0x0000;
 
         /// <summary>
         /// T=0 is the active protocol.
         /// </summary>
-        public const Int16 SCARD_PROTOCOL_T0 = 0x0001;
+        public const short SCARD_PROTOCOL_T0 = 0x0001;
 
         /// <summary>
         /// T=1 is the active protocol.
         /// </summary>
-        public const Int16 SCARD_PROTOCOL_T1 = 0x0002;
+        public const short SCARD_PROTOCOL_T1 = 0x0002;
 
         /// <summary>
         /// Raw is the active protocol.
         /// </summary>
-        public const Int16 SCARD_PROTOCOL_RAW = 0x0004;
+        public const short SCARD_PROTOCOL_RAW = 0x0004;
         #endregion
 
         #region Reader state
@@ -622,7 +622,7 @@ namespace CardTest
         /// <summary>
         /// Infinite timeout
         /// </summary>
-        public const UInt32 INFINITE = 0xFFFFFFFF;
+        public const uint INFINITE = 0xFFFFFFFF;
 
         #region Imported methods
 
@@ -681,84 +681,84 @@ namespace CardTest
         public static extern uint SCardControl(int hCard, uint dwControlCode, ref byte SendBuff, int SendBuffLen, ref byte RecvBuff, int RecvBuffLen, ref int pcbBytesReturned);
 
         [DllImport("winscard.dll")]
-        public static extern uint SCardGetStatusChange(int hContext, UInt32 TimeOut, [In, Out] SCARD_READERSTATE[] ReaderState, int ReaderCount);
+        public static extern uint SCardGetStatusChange(int hContext, uint TimeOut, [In, Out] SCARD_READERSTATE[] ReaderState, int ReaderCount);
         #endregion
         public static string GetScardErrMsg(uint ReturnCode)
         {
             switch (ReturnCode)
             {
                 case SCARD_E_CANCELLED:
-                    return ("The action was canceled by an SCardCancel request.");
+                    return "The action was canceled by an SCardCancel request.";
                 case SCARD_E_CANT_DISPOSE:
-                    return ("The system could not dispose of the media in the requested manner.");
+                    return "The system could not dispose of the media in the requested manner.";
                 case SCARD_E_CARD_UNSUPPORTED:
-                    return ("The smart card does not meet minimal requirements for support.");
+                    return "The smart card does not meet minimal requirements for support.";
                 case SCARD_E_DUPLICATE_READER:
-                    return ("The reader driver didn't produce a unique reader name.");
+                    return "The reader driver didn't produce a unique reader name.";
                 case SCARD_E_INSUFFICIENT_BUFFER:
-                    return ("The data buffer for returned data is too small for the returned data.");
+                    return "The data buffer for returned data is too small for the returned data.";
                 case SCARD_E_INVALID_ATR:
-                    return ("An ATR string obtained from the registry is not a valid ATR string.");
+                    return "An ATR string obtained from the registry is not a valid ATR string.";
                 case SCARD_E_INVALID_HANDLE:
-                    return ("The supplied handle was invalid.");
+                    return "The supplied handle was invalid.";
                 case SCARD_E_INVALID_PARAMETER:
-                    return ("One or more of the supplied parameters could not be properly interpreted.");
+                    return "One or more of the supplied parameters could not be properly interpreted.";
                 case SCARD_E_INVALID_TARGET:
-                    return ("Registry startup information is missing or invalid.");
+                    return "Registry startup information is missing or invalid.";
                 case SCARD_E_INVALID_VALUE:
-                    return ("One or more of the supplied parameter values could not be properly interpreted.");
+                    return "One or more of the supplied parameter values could not be properly interpreted.";
                 case SCARD_E_NOT_READY:
-                    return ("The reader or card is not ready to accept commands.");
+                    return "The reader or card is not ready to accept commands.";
                 case SCARD_E_NOT_TRANSACTED:
-                    return ("An attempt was made to end a non-existent transaction.");
+                    return "An attempt was made to end a non-existent transaction.";
                 case SCARD_E_NO_MEMORY:
-                    return ("Not enough memory available to complete this command.");
+                    return "Not enough memory available to complete this command.";
                 case SCARD_E_NO_SERVICE:
-                    return ("The smart card resource manager is not running.");
+                    return "The smart card resource manager is not running.";
                 case SCARD_E_NO_SMARTCARD:
-                    return ("The operation requires a smart card, but no smart card is currently in the device.");
+                    return "The operation requires a smart card, but no smart card is currently in the device.";
                 case SCARD_E_PCI_TOO_SMALL:
-                    return ("The PCI receive buffer was too small.");
+                    return "The PCI receive buffer was too small.";
                 case SCARD_E_PROTO_MISMATCH:
-                    return ("The requested protocols are incompatible with the protocol currently in use with the card.");
+                    return "The requested protocols are incompatible with the protocol currently in use with the card.";
                 case SCARD_E_READER_UNAVAILABLE:
-                    return ("The specified reader is not currently available for use.");
+                    return "The specified reader is not currently available for use.";
                 case SCARD_E_READER_UNSUPPORTED:
-                    return ("The reader driver does not meet minimal requirements for support.");
+                    return "The reader driver does not meet minimal requirements for support.";
                 case SCARD_E_SERVICE_STOPPED:
-                    return ("The smart card resource manager has shut down.");
+                    return "The smart card resource manager has shut down.";
                 case SCARD_E_SHARING_VIOLATION:
-                    return ("The smart card cannot be accessed because of other outstanding connections.");
+                    return "The smart card cannot be accessed because of other outstanding connections.";
                 case SCARD_E_SYSTEM_CANCELLED:
-                    return ("The action was canceled by the system, presumably to log off or shut down.");
+                    return "The action was canceled by the system, presumably to log off or shut down.";
                 case SCARD_E_TIMEOUT:
-                    return ("The user-specified timeout value has expired.");
+                    return "The user-specified timeout value has expired.";
                 case SCARD_E_UNKNOWN_CARD:
-                    return ("The specified smart card name is not recognized.");
+                    return "The specified smart card name is not recognized.";
                 case SCARD_E_UNKNOWN_READER:
-                    return ("The specified reader name is not recognized.");
+                    return "The specified reader name is not recognized.";
                 case SCARD_F_COMM_ERROR:
-                    return ("An internal communications error has been detected.");
+                    return "An internal communications error has been detected.";
                 case SCARD_F_INTERNAL_ERROR:
-                    return ("An internal consistency check failed.");
+                    return "An internal consistency check failed.";
                 case SCARD_F_UNKNOWN_ERROR:
-                    return ("An internal error has been detected, but the source is unknown.");
+                    return "An internal error has been detected, but the source is unknown.";
                 case SCARD_F_WAITED_TOO_LONG:
-                    return ("An internal consistency timer has expired.");
+                    return "An internal consistency timer has expired.";
                 case SCARD_S_SUCCESS:
-                    return ("OK");
+                    return "OK";
                 case SCARD_W_REMOVED_CARD:
-                    return ("The smart card has been removed, so that further communication is not possible.");
+                    return "The smart card has been removed, so that further communication is not possible.";
                 case SCARD_W_RESET_CARD:
-                    return ("The smart card has been reset, so any shared state information is invalid.");
+                    return "The smart card has been reset, so any shared state information is invalid.";
                 case SCARD_W_UNPOWERED_CARD:
-                    return ("Power has been removed from the smart card, so that further communication is not possible.");
+                    return "Power has been removed from the smart card, so that further communication is not possible.";
                 case SCARD_W_UNRESPONSIVE_CARD:
-                    return ("The smart card is not responding to a reset.");
+                    return "The smart card is not responding to a reset.";
                 case SCARD_W_UNSUPPORTED_CARD:
-                    return ("The reader cannot communicate with the card, due to ATR string configuration conflicts.");
+                    return "The reader cannot communicate with the card, due to ATR string configuration conflicts.";
                 default:
-                    return ("?");
+                    return "?";
             }
         }
     }
